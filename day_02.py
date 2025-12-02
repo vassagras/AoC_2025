273c70eb6580928eae5bc9c98ffb40345a9a6cc7
agras7 <vassilis.agrafiotis@gmail.com>
2025-12-02 22:21:26 +0100

    Day 2, Part 2

--- day_02.py
+++ day_02.py
@@ -8,24 +8,36 @@ def find_invalid_id(identifier) -> bool:
             return True
     return False
 
+def find_invalid_id_v2(identifier) -> bool:
+    num = str(identifier)
+    l = len(num)
+    half = l // 2
+    for j in range(1,half+1,1):
+        pattern = num[0:j]
+        chunks = [num[k:k+j] for k in range(j, l, j)]
+        if all(p == pattern for p in chunks): return True
+
+    return False
+
 start_p1 = time.time()
-file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), r"inputs\Day 02"), 'r')
+file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), r"data\Day 02"), 'r')
 
 sum_1 = 0
+sum_2 = 0
 ranges = [i.strip() for i in file.read().split(",")]
 
 for r in ranges:
     s = int(r.split("-")[0])
     e = int(r.split("-")[1])
-    # print("Range from {} to {}".format(s, e))
     for i in range(s, e+1, 1):
         if find_invalid_id(i):
             sum_1 += i
-            # print(i)
+        if find_invalid_id_v2(i):
+            sum_2 += i
 
 end_p1 = time.time()
 
 print("Part 1 result: {}".format(sum_1))
+print("Part 1 result: {}".format(sum_2))
 print("The time of execution of above program is :", (end_p1 - start_p1) * 10**3, "ms")
 print()
-
