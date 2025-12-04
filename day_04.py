b304add3baca86aa4b50cb8cd163d5b241bde8d8
agras7 <vassilis.agrafiotis@gmail.com>
2025-12-04 23:45:37 +0100

    Day 4, Part 2

--- day_04.py
+++ day_04.py
@@ -30,18 +30,29 @@ file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), r"data\Day 
 
 count_1 = 0
 count_2 = 0
-grid = []
-
-for l in file.readlines():
-    grid.append([c for c in l.strip()])
+grid = [list(line.strip()) for line in file]
 
 for i in range(len(grid)):
     for j in range(len(grid[0])):
-        if is_weak_spot(grid, i, j): count_1 += 1
+        if is_weak_spot(grid, i, j):
+            count_1 += 1
+
+count_2 = 0
+while True:
+    weak_spots = [(i, j) for i in range(len(grid)) for j in range(len(grid[0]))
+                  if is_weak_spot(grid, i, j)]
+    if not weak_spots:
+        break
+
+    count_2 += len(weak_spots)
+
+    for x, y in weak_spots:
+        grid[x][y] = "."
+
 
 end_p1 = time.time()
 
 print("Part 1 result: {}".format(count_1))
-print("Part 1 result: {}".format(count_2))
+print("Part 2 result: {}".format(count_2))
 print("The time of execution of above program is :", (end_p1 - start_p1) * 10**3, "ms")
 print()
