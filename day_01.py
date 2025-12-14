a2a7101ec09fe10f24c1b5c2f000cc2841d8392a
agras7 <vassilis.agrafiotis@gmail.com>
2025-12-14 15:29:45 +0100

    Day 1, minor code refactoring

--- day_01.py
+++ day_01.py
@@ -6,19 +6,25 @@ file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), r"data\Day 
 
 dial_position = 50
 count_of_zero_dials = 0
-count_of_zero_passes = 0
+count_zero_passes = 0
 
 print("The dial starts by pointing at {}".format(dial_position))
 for line in file:
-    op = line[:1]
+    op = line[0]
     rotations = int(line[1:].strip())
 
-    if op == "L":
-        count_of_zero_passes += max(0, (rotations - dial_position + 99) // 100)
-        dial_position = (dial_position - rotations) % 100
-    elif op == "R":
-        count_of_zero_passes += (dial_position + rotations) // 100
-        dial_position = (dial_position + rotations) % 100
+    zero_passes = 0
+    new_position = 0
+
+    if op == "R":
+        new_position = (dial_position + rotations) % 100
+        zero_passes = (dial_position + rotations) // 100
+    elif op == "L":
+        new_position = (dial_position - rotations) % 100
+        zero_passes = (rotations - dial_position - 1) // 100 + 1 if rotations >= dial_position else 0
+
+    count_zero_passes += zero_passes
+    dial_position = new_position
 
     if dial_position == 0: count_of_zero_dials += 1
     print("The dial is rotated {} to point at {}".format(line.strip(), dial_position))
@@ -26,6 +32,6 @@ for line in file:
 end_time = time.time()
 
 print("Part 1 result: {}".format(count_of_zero_dials))
-print("Part 2 result: {}".format(count_of_zero_passes))
+print("Part 2 result: {}".format(count_zero_passes))
 print("The time of execution of above program is :", (end_time - start_time) * 10**3, "ms")
 print()
